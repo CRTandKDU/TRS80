ca4d66eacdb91c7604c735b555a7a9929f7f1eff
CRTandKDU <jeanmariechauvet@gmail.com>
2014-05-08 16:53:03 +0200

    Fixed skipping blank chars between keywords. Fixed byte count in line records for REM v. <quote>. FIXED "TAB(" keyword.

--- bl2.el
+++ bl2.el
@@ -10,13 +10,13 @@
 
 ;; Lexer
 (defconst *bl2-tokens* 
-  '(("END" . 128) ("FOR" . 129) ("KILL" . 170) ("OR" . 211) ("RESET" . 130) ("LSET" . 171) (">" . 212) ("SET" . 131) ("RSET" . 172) ("=" . 213) ("CLS" . 132) ("SAVE" . 173) ("<" . 214) ("CMD" . 133) ("SYSTEM" . 174) ("SGN" . 215) ("RANDOM" . 134) ("LPRINT" . 175) ("INT" . 216) ("NEXT" . 135) ("DEF" . 176) ("ABS" . 217) ("DATA" . 136) ("POKE" . 177) ("FRE" . 218) ("INPUT" . 137) ("PRINT" . 178) ("INP" . 219) ("DIM" . 138) ("CONT" . 179) ("POS" . 220) ("READ" . 139) ("LIST" . 180) ("SQR" . 221) ("LET" . 140) ("LLIST" . 181) ("RND" . 222) ("GOTO" . 141) ("DELETE" . 182) ("LOG" . 223) ("RUN" . 142) ("AUTO" . 183) ("EXP" . 224) ("IF" . 143) ("CLEAR" . 184) ("COS" . 225) ("RESTORE" . 144) ("CLOAD" . 185) ("SIN" . 226) ("GOSUB" . 145) ("CSAVE" . 186) ("TAN" . 227) ("RETURN" . 146) ("NEW" . 187) ("ATN" . 228) ("REM" . 147) ("TAB" . 188) ("PEEK" . 229) ("STOP" . 148) ("TO" . 189) ("CVI" . 230) ("ELSE" . 149) ("FN" . 190) ("CVS" . 231) ("TRON" . 150) ("USING" . 191) ("CVD" . 232) ("TROFF" . 151) ("VARPTR" . 192) ("EOF" . 233) ("DEFSTR" . 152) ("USR" . 193) ("LOC" . 234) ("DEFINT" . 153) ("ERL" . 194) ("LOF" . 235) ("DEFSNG" . 154) ("ERR" . 195) ("MKI$" . 236) ("DEFDBL" . 155) ("STRING$" . 196) ("MKS$" . 237) ("LINE" . 156) ("INSTR" . 197) ("MKD$" . 238) ("EDIT" . 157) ("POINT" . 198) ("CINT" . 239) ("ERROR" . 158) ("TIME$" . 199) ("CSNG" . 240) ("RESUME" . 159) ("MEM" . 200) ("CDBL" . 241) ("OUT" . 160) ("INKEY$" . 201) ("FIX" . 242) ("ON" . 161) ("THEN" . 202) ("LEN" . 243) ("OPEN" . 162) ("NOT" . 203) ("STR$" . 244) ("FIELD" . 163) ("STEP" . 204) ("VAL" . 245) ("GET" . 164) ("+" . 205) ("ASC" . 246) ("PUT" . 165) ("-" . 206) ("CHR$" . 247) ("CLOSE" . 166) ("*" . 207) ("LEFT$" . 248) ("LOAD" . 167) ("/" . 208) ("RIGHT$" . 249) ("MERGE" . 168) ("^" . 209) ("MID$" . 250) ("NAME" . 169) ("AND" . 210) ("'" 58 147 251))
+  '(("END" . 128) ("FOR" . 129) ("KILL" . 170) ("OR" . 211) ("RESET" . 130) ("LSET" . 171) (">" . 212) ("SET" . 131) ("RSET" . 172) ("=" . 213) ("CLS" . 132) ("SAVE" . 173) ("<" . 214) ("CMD" . 133) ("SYSTEM" . 174) ("SGN" . 215) ("RANDOM" . 134) ("LPRINT" . 175) ("INT" . 216) ("NEXT" . 135) ("DEF" . 176) ("ABS" . 217) ("DATA" . 136) ("POKE" . 177) ("FRE" . 218) ("INPUT" . 137) ("PRINT" . 178) ("INP" . 219) ("DIM" . 138) ("CONT" . 179) ("POS" . 220) ("READ" . 139) ("LIST" . 180) ("SQR" . 221) ("LET" . 140) ("LLIST" . 181) ("RND" . 222) ("GOTO" . 141) ("DELETE" . 182) ("LOG" . 223) ("RUN" . 142) ("AUTO" . 183) ("EXP" . 224) ("IF" . 143) ("CLEAR" . 184) ("COS" . 225) ("RESTORE" . 144) ("CLOAD" . 185) ("SIN" . 226) ("GOSUB" . 145) ("CSAVE" . 186) ("TAN" . 227) ("RETURN" . 146) ("NEW" . 187) ("ATN" . 228) ("REM" . 147) ("TAB(" . 188) ("PEEK" . 229) ("STOP" . 148) ("TO" . 189) ("CVI" . 230) ("ELSE" . 149) ("FN" . 190) ("CVS" . 231) ("TRON" . 150) ("USING" . 191) ("CVD" . 232) ("TROFF" . 151) ("VARPTR" . 192) ("EOF" . 233) ("DEFSTR" . 152) ("USR" . 193) ("LOC" . 234) ("DEFINT" . 153) ("ERL" . 194) ("LOF" . 235) ("DEFSNG" . 154) ("ERR" . 195) ("MKI$" . 236) ("DEFDBL" . 155) ("STRING$" . 196) ("MKS$" . 237) ("LINE" . 156) ("INSTR" . 197) ("MKD$" . 238) ("EDIT" . 157) ("POINT" . 198) ("CINT" . 239) ("ERROR" . 158) ("TIME$" . 199) ("CSNG" . 240) ("RESUME" . 159) ("MEM" . 200) ("CDBL" . 241) ("OUT" . 160) ("INKEY$" . 201) ("FIX" . 242) ("ON" . 161) ("THEN" . 202) ("LEN" . 243) ("OPEN" . 162) ("NOT" . 203) ("STR$" . 244) ("FIELD" . 163) ("STEP" . 204) ("VAL" . 245) ("GET" . 164) ("+" . 205) ("ASC" . 246) ("PUT" . 165) ("-" . 206) ("CHR$" . 247) ("CLOSE" . 166) ("*" . 207) ("LEFT$" . 248) ("LOAD" . 167) ("/" . 208) ("RIGHT$" . 249) ("MERGE" . 168) ("^" . 209) ("MID$" . 250) ("NAME" . 169) ("AND" . 210) ("'" 58 147 251))
   "BASIC Level 2 Tokens"
   )
 
 (defun bl2-lex-match-token ()
   "Search for exact token match at char"
-  (skip-chars-forward " \t")
+  ;; (skip-chars-forward " \t")
   (or
    (and
     (<= (+ 7 (point)) (point-max))
@@ -92,7 +92,10 @@
 				   (if (listp (cdr match)) 
 				       (cdr match)
 				     (list (cdr match))))
-		    byte-count (1+ byte-count))
+		    byte-count (if (listp (cdr match))
+				   (+ byte-count (length (cdr match)))
+				 (1+ byte-count))
+		    )
 	      ;; Special statements
 	      (cond
 	       ;; `REM', `'': pass through to end of line
